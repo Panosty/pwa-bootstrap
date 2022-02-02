eec2dd3618de356d056213a8f497c2850515827c
Panosty Blanco <blancolouis1287@gmail.com>
2022-02-02 15:40:16 -0500

    Modification finale labo2

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
- };
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js');
+  console.log('registered successfully')
+};
+
+
+Toasty();
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+
+  // Prevent the mini-infobar from appearing on mobile
+
+  e.preventDefault();
+
+  // Stash the event so it can be triggered later.
+
+  deferredPrompt = e;
+
+  // Update UI notify the user they can install the PWA
+
+   // showInstallPromotion();
+
+  // Optionally, send analytics event that PWA install promo was shown.
+
+  console.log(`'beforeinstallprompt' event was fired.`);
+
  
+
+  const installAppButton = document.getElementById('installAppButton');
+  if (installAppButton) {
+    installAppButton.addEventListener('click', function () {
+      deferredPrompt.prompt();
+    })
+
+  }
+
+});
+
+var option = {
+
+  animation: true,
+
+  delay: 4000
+
+};
+
+function Toasty() {
+
+  var toastHTMLElement = document.getElementById("Aplication");
+
+  var toastElement = new bootstrap.Toast(toastHTMLElement, option);
+
+  toastElement.show();
+
+}
--- sw.js
+++ sw.js
@@ -2,23 +2,17 @@ this.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('v1').then(function(cache) {
         return cache.addAll([
-          '/pwa-bootsrtap/',
-           '/pwa-bootsrtap/index.html',
-           '/pwa-bootsrtap/index.js',
-           '/pwa-bootsrtap/bootstrap-5.1.3-dist/css/bootstrap.min.css',
-           '/pwa-bootsrtap/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-           '/pwa-bootsrtap/recettes/img1.jpg',
-           '/pwa-bootsrtap/recettes/img2.jpg',
-           '/pwa-bootsrtap/recettes/img3.jpg',
-           '/pwa-bootsrtap/icon-152x152.png',
-           '/pwa-bootsrtap/icons-1.7.2/font/bootstrap-icons.css',
-           '/pwa-bootsrtap/favicon.ico'
-           /* '/restaurant32x32.png',
-           '/restaurant48x48.png',
-           '/restaurant152x152.png',
-           '/restaurant192x192.png',
-           '/restaurant512x512.png',
-           '/manifest.webmanifest' */
+           '/pwa-bootstrap/index.html',
+           '/pwa-bootstrap/index.js',
+           '/pwa-bootstrap/bootstrap-5.1.3-dist/css/bootstrap.min.css',
+           '/pwa-bootstrap/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+           '/pwa-bootstrap/recettes/img1.jpg',
+           '/pwa-bootstrap/recettes/img2.jpg',
+           '/pwa-bootstrap/recettes/img3.jpg',
+           '/pwa-bootstrap/restaurant.png',
+           '/pwa-bootstrap/icons-1.7.2/font/bootstrap-icons.css',
+           '/pwa-bootstrap/boot.png',
+           '/pwa-bootstrap/icon-192x192.png',
         ]);
       })
     );
